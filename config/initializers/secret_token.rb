32688dd33d8d6b7577574e52c0c69d57926e9c8f
wadror <wadror@wadror-VirtualBox.(none)>
2015-01-18 16:32:07 +0200

    rails asennettu jesjesjes console toimii

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ratebeer::Application.config.secret_key_base = '5d35f53fcaf80536c4451bc8ef7240d9d589f9a2446ab2093e51cb85593cd423890931fd443e33597339697ad0b54d12b6ff3b7a77d490f7b24ddebdbe8138e7'
